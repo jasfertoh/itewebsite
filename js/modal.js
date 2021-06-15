dcdd2e0a7bc0093f9faf42213391d37020af12fd
Jasfer <59435921+jasfertoh@users.noreply.github.com>
2021-06-15 18:23:12 +0800

    Added files

---
+++ js/modal.js
@@ -0,0 +1,203 @@
+window.onload = function() {
+    var modal0 = document.getElementById("myModal");
+
+    var btn0 = document.getElementById("myBtn");
+    
+    var span0 = document.getElementsByClassName("close")[0];
+
+    btn0.onclick = function() {
+        modal0.style.display = "block";
+    }
+
+    span0.onclick = function() {
+        modal0.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal0) {
+            modal0.style.display = "none";
+        }
+    }
+
+    var modal1 = document.getElementById("myModal1");
+
+    var btn1 = document.getElementById("myBtn1");
+
+    var span1 = document.getElementsByClassName("close")[1];
+    console.log(span1)
+
+    btn1.onclick = function() {
+        modal1.style.display = "block";
+    }
+
+    span1.onclick = function() {
+        modal1.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal1) {
+            modal1.style.display = "none";
+        }
+    }
+
+    var modal2 = document.getElementById("myModal2");
+
+    var btn2 = document.getElementById("myBtn2");
+
+    var span2 = document.getElementsByClassName("close")[2];
+    
+
+    btn2.onclick = function() {
+        modal2.style.display = "block";
+    }
+
+    span2.onclick = function() {
+        modal2.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal2) {
+            modal2.style.display = "none";
+        }
+    }
+
+    var modal3 = document.getElementById("myModal3");
+
+    var btn3 = document.getElementById("myBtn3");
+    
+    var span3 = document.getElementsByClassName("close")[3];
+
+    btn3.onclick = function() {
+        modal3.style.display = "block";
+    }
+
+    span3.onclick = function() {
+        modal3.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal3) {
+            modal3.style.display = "none";
+        }
+    }
+
+    var modal4 = document.getElementById("myModal4");
+
+    var btn4 = document.getElementById("myBtn4");
+
+    var span4 = document.getElementsByClassName("close")[4];
+
+    btn4.onclick = function() {
+        modal4.style.display = "block";
+    }
+
+    span4.onclick = function() {
+        modal4.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal4) {
+            modal4.style.display = "none";
+        }
+    }
+
+    var modal5 = document.getElementById("myModal5");
+
+    var btn5 = document.getElementById("myBtn5");
+    
+    var span5 = document.getElementsByClassName("close")[5];
+
+    btn5.onclick = function() {
+        modal5.style.display = "block";
+    }
+
+    span5.onclick = function() {
+        modal5.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal5) {
+            modal5.style.display = "none";
+        }
+    }
+
+    var modal6 = document.getElementById("myModal6");
+
+    var btn6 = document.getElementById("myBtn6");
+    
+    var span6 = document.getElementsByClassName("close")[6];
+
+    btn6.onclick = function() {
+        modal6.style.display = "block";
+    }
+
+    span6.onclick = function() {
+        modal6.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal6) {
+            modal6.style.display = "none";
+        }
+    }
+
+    var modal7 = document.getElementById("myModal7");
+
+    var btn7 = document.getElementById("myBtn7");
+    
+    var span7 = document.getElementsByClassName("close")[7];
+
+    btn7.onclick = function() {
+        modal7.style.display = "block";
+    }
+
+    span7.onclick = function() {
+        modal7.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal7) {
+            modal7.style.display = "none";
+        }
+    }
+
+    var modal8 = document.getElementById("myModal8");
+
+    var btn8 = document.getElementById("myBtn8");
+    
+    var span8 = document.getElementsByClassName("close")[8];
+
+    btn8.onclick = function() {
+        modal8.style.display = "block";
+    }
+
+    span8.onclick = function() {
+        modal8.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal8) {
+            modal8.style.display = "none";
+        }
+    }
+
+    var modal9 = document.getElementById("myModal9");
+
+    var btn9 = document.getElementById("myBtn9");
+    
+    var span9 = document.getElementsByClassName("close")[9];
+
+    btn9.onclick = function() {
+        modal9.style.display = "block";
+    }
+
+    span9.onclick = function() {
+        modal9.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal9) {
+            modal9.style.display = "none";
+        }
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    document.getElementById('btn').onclick = function() {
+        window.open('https://www.asiaone.com/singapore/waterfall-ite-college-central-roof-partly-gives-way-due-rainwater')
+    }
+
+    document.getElementById('btn1').onclick = function() {
+        window.open('https://www.straitstimes.com/singapore/parenting-education/staff-at-ite-polys-get-appreciation-day-in-november')
+    }
+
+    document.getElementById('btn2').onclick = function() {
+        window.open('https://www.straitstimes.com/singapore/education/digital-skills-will-not-be-sufficient-in-future-edb-chief-chng-kai-fong')
+    }
+
+    document.getElementById('btn3').onclick = function() {
+        window.open('https://www.straitstimes.com/sport/nearly-400-ite-students-and-staff-get-ready-for-st-run')
+    }
+}
